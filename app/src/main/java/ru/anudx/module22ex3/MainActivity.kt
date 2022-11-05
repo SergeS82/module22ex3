a36a4427767256b76156c151d815fcc706397ea2
SergeS82 <bazantmail@gmail.com>
2022-11-05 23:02:44 +0300

    Анимация дочерних элеменов при динамическом создании

---
+++ app/src/main/java/ru/anudx/module22ex3/MainActivity.kt
@@ -0,0 +1,38 @@
+package ru.anudx.module22ex3
+
+import android.animation.AnimatorInflater
+import android.animation.LayoutTransition
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import ru.anudx.module22ex3.databinding.ActivityMainBinding
+import ru.anudx.module22ex3.databinding.ButtonNewBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
+    private lateinit var t: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        b.linearLayout.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        b.linearLayout.layoutTransition.setAnimator(LayoutTransition.APPEARING, AnimatorInflater.loadAnimator(this, R.anim.anim_liner))
+        b.btnAdd.setOnClickListener {
+            //val button = findViewById<Button>(R.id.bnt_new)
+            val button = Button(this)
+            button.text = "QWERTY"
+            button.scaleX = 0f
+            button.scaleY = 0f
+            button.setOnClickListener {
+                (it as Button).text = "1234567890"
+            }
+            b.linearLayout.addView(button)
+        }
+        b.btnDel.setOnClickListener {
+            if (b.linearLayout.childCount > 0){
+                b.linearLayout.removeViewAt(b.linearLayout.childCount-1)
+            }
+        }
+    }
+}
